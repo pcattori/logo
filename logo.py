06f3a5bf994f779d6b2873406ebfd5b7c2471745
Pedro Cattori <pcattori@gmail.com>
2017-02-08 17:45:12 -0500

    initial logo

---
+++ logo.py
@@ -0,0 +1,28 @@
+import collections
+scale = 128
+stroke_width = 48
+Point = collections.namedtuple('Point', ['x', 'y'])
+origin = Point(0, 128)
+
+backwards_s = ' '.join([
+    f'M{origin.x},{origin.y}',
+    f'l {2*scale + stroke_width/2},0',
+    f'a{scale},{scale} 0 0,1 0,{2 * scale}',
+    f'l -{scale},0',
+    f'a{scale},{scale} 0 0,0 0,{2 * scale}',
+    f'l {2*scale + stroke_width/2},0'])
+
+vertical_line = f'M{origin.x},{origin.y} m{1.5*scale + stroke_width/2},{scale} l 0,{2 * scale}'
+
+path_d = f'{backwards_s} {vertical_line}'
+
+
+svg = f'''<?xml version="1.0" standalone="no"?>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 1200" version="1.1">
+    <title>PC</title>
+    <desc>PC logo</desc>
+    <path d="{path_d}" fill="none" stroke="black" stroke-width="{stroke_width}"/>
+</svg>
+'''
+
+print(svg)
