6a199fc0c50859acc684d9ceb7e57bced5f68382
Pedro Cattori <pcattori@gmail.com>
2017-02-09 12:34:20 -0500

    logo + symbol

--- logo.py
+++ logo.py
@@ -16,14 +16,21 @@ vertical_line = f'M{origin.x},{origin.y} m{1.5*scale + stroke_width/2},{scale} l
 
 path_d = f'{backwards_s} {vertical_line}'
 
+symbol_svg = f'''<?xml version="1.0" standalone="no"?>
+<svg
+        xmlns="http://www.w3.org/2000/svg"
+        version="1.1" style="display:none">
+    <symbol id="pcattori-logo" viewBox="0 0 {3*scale + stroke_width} {4*scale + stroke_width}">
+        <path d="{path_d}" fill="none" stroke="black" stroke-width="{stroke_width}"/>
+    </symbol>
+</svg>
+'''
 
 svg = f'''<?xml version="1.0" standalone="no"?>
 <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 {3*scale + stroke_width} {4*scale + stroke_width}"
         version="1.1">
-    <title>PC</title>
-    <desc>PC logo</desc>
     <path d="{path_d}" fill="none" stroke="black" stroke-width="{stroke_width}"/>
 </svg>
 '''
